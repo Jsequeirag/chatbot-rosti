3374717daa99e6ec8df2d155a7f392bdaf2f95d0
Jsequeirag <99283803+Jsequeirag@users.noreply.github.com>
2024-12-11 22:00:54 -0600

    full-app

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,7 @@ export default {
   output: {
     file: "dist/app.js",
     format: "esm",
+    sourcemap: "inline",
   },
   onwarn: (warning) => {
     if (warning.code === "UNRESOLVED_IMPORT") return;
